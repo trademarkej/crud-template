e3687a01b6af308573df64d491663640229edd67
patrick_hewes <patrick.hewes@centeva.com>
2021-12-01 17:54:54 -0600

    Add nodemon to scripts in package.json

--- index.js
+++ index.js
@@ -4,6 +4,13 @@ const server = express();
 
 const PORT = 5000;
 
+// Start by running the following command in the terminal:
+// npm run server
+
+server.get('/', (req, res) => {
+    res.json({ hello: "world" });
+});
+
 server.listen(5000, () => {
     console.log(`\n*** Server Running on http://localhost:${PORT}`);
 });
