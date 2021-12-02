ed906b1bab5d83c76c5aab16c344954876368601
patrick_hewes <patrick.hewes@centeva.com>
2021-12-02 16:55:02 -0600

    Add put and patch routes for lessons & channels for Lesson 7

--- index.js
+++ index.js
@@ -19,6 +19,20 @@ server.get('/', (req, res) => {
 });
 
 /***** Channels Routes *****/
+server.delete("/api/channels/:id", (req, res) => {
+    const { id } = req.params;
+
+    const deleted = channels.find(channel => channel.id === id);
+    if (deleted) {
+        channels = channels.find(channel => channel.id !== id);
+        res.status(200).json(deleted);
+    } else {
+        res
+            .status(404)
+            .json({ message: "Channel you are looking for does not exist" });
+    }
+});
+
 server.get("/api/channels", (req, res) => {
     res.status(200).json(channels);
 });
@@ -34,6 +48,20 @@ server.get("/api/channels/:id", (req, res) => {
     }
 });
 
+server.patch("/api/channels/:id", (req, res) => {
+    const { id } = req.params;
+    changes = req.body;
+
+    const found = channels.find(channel => channel.id === id);
+
+    if (found) {
+        Object.assign(found, changes);
+        res.status(200).json(found);
+    } else {
+        res.status(404).json({ message: "channel does not exist" });
+    }
+});
+
 server.post('/api/channels', (req, res) => {
     const channelInfo = req.body;
     channelInfo.id = shortid.generate();
@@ -41,21 +69,35 @@ server.post('/api/channels', (req, res) => {
     res.status(201).json(channelInfo);
 });
 
-server.delete("/api/channels/:id", (req, res) => {
+server.put("/api/channels/:id", (req, res) => {
     const { id } = req.params;
+    const changes = req.body;
 
-    const deleted = channels.find(channel => channel.id === id);
-    if (deleted) {
-        channels = channels.find(channel => channel.id !== id);
-        res.status(200).json(deleted);
+    const index = channels.findIndex(channel => channel.id === id);
+
+    if (index !== -1) {
+        channels[index] = changes;
+        res.status(200).json(channels[index]);
     } else {
-        res
-            .status(404)
-            .json({ message: "Channel you are looking for does not exist" });
+        res.status(404).json({ message: "channel does not exist" });
     }
 });
 
 /***** Lessons Routes *****/
+server.delete("/api/lessons/:id", (req, res) => {
+    const { id } = req.params;
+
+    const deleted = lessons.find(lesson => lesson.id === id);
+    if (deleted) {
+        lessons = lessons.find(lesson => lesson.id !== id)
+        res.status(200).json(deleted);
+    } else {
+        res
+            .status(404)
+            .json({ message: "Lesson you are looking for does not exist" });
+    }
+});
+
 server.get("/api/lessons", (req, res) => {
     res.status(200).json(lessons);
 });
@@ -71,6 +113,20 @@ server.get("/api/lessons/:id", (req, res) => {
     }
 });
 
+server.patch("/api/lessons/:id", (req, res) => {
+    const { id } = req.params;
+    changes = req.body;
+
+    const found = lessons.find(lesson => lesson.id === id);
+
+    if (found) {
+        Object.assign(found, changes);
+        res.status(200).json(found);
+    } else {
+        res.status(404).json({ message: "lesson does not exist" });
+    }
+});
+
 server.post('/api/lessons', (req, res) => {
     const lessonInfo = req.body;
     lessonInfo.id = shortid.generate();
@@ -78,17 +134,17 @@ server.post('/api/lessons', (req, res) => {
     res.status(201).json(lessonInfo);
 });
 
-server.delete("/api/lessons/:id", (req, res) => {
+server.put("/api/lessons/:id", (req, res) => {
     const { id } = req.params;
+    const changes = req.body;
 
-    const deleted = lessons.find(lesson => lesson.id === id);
-    if (deleted) {
-        lessons = lessons.find(lesson => lesson.id !== id)
-        res.status(200).json(deleted);
+    const index = lessons.findIndex(lesson => lesson.id === id);
+
+    if (index !== -1) {
+        lessons[index] = changes;
+        res.status(200).json(lessons[index]);
     } else {
-        res
-            .status(404)
-            .json({ message: "Lesson you are looking for does not exist" });
+        res.status(404).json({ message: "lesson does not exist" });
     }
 });
 
