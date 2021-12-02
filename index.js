005ef77e9118d75874c322c80f0cb672f93472d4
patrick_hewes <patrick.hewes@centeva.com>
2021-12-02 11:16:37 -0600

    Add channels post action from lesson 4

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
-const express = require("express");
+const express = require('express');
+const shortid = require('shortid');
 
 const server = express();
 
+server.use(express.json());
+
 const PORT = 5000;
+
+const channels = [];
+
+const lessons = [];
 
 // Start by running the following command in the terminal:
 // npm run server
 
 server.get('/', (req, res) => {
     res.json({ hello: "world" });
+});
+
+server.post('/api/channels', (req, res) => {
+    const channelInfo = req.body;
+    channelInfo.id = shortid.generate();
+    channels.push(channelInfo);
+    res.status(201).json(channelInfo);
 });
 
 server.listen(5000, () => {
