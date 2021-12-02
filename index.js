5ac21fb347bea2aba8df98e61b627d05e23d0024
patrick_hewes <patrick.hewes@centeva.com>
2021-12-02 12:49:41 -0600

    Add basic get routes for lesson and channels by id for Lesson 6

--- index.js
+++ index.js
@@ -18,8 +18,20 @@ server.get('/', (req, res) => {
     res.json({ hello: "world" });
 });
 
+/***** Channels Routes *****/
 server.get("/api/channels", (req, res) => {
     res.status(200).json(channels);
+});
+
+server.get("/api/channels/:id", (req, res) => {
+    const { id } = req.params;
+    const found = channels.find(channel => channel.id === id);
+
+    if (found) {
+        res.status(200).json(found);
+    } else {
+        res.status(404).json({ message: "channel does not exist" });
+    }
 });
 
 server.post('/api/channels', (req, res) => {
@@ -43,8 +55,20 @@ server.delete("/api/channels/:id", (req, res) => {
     }
 });
 
+/***** Lessons Routes *****/
 server.get("/api/lessons", (req, res) => {
     res.status(200).json(lessons);
+});
+
+server.get("/api/lessons/:id", (req, res) => {
+    const { id } = req.params;
+    const found = lessons.find(lesson => lesson.id === id);
+
+    if (found) {
+        res.status(200).json(found);
+    } else {
+        res.status(404).json({ message: "lesson does not exist" });
+    }
 });
 
 server.post('/api/lessons', (req, res) => {
