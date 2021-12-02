27d9e85dc22ccb7a0de118056bbad9dbbaa5361b
Patrick <trademarkej@users.noreply.github.com>
2021-12-02 12:33:20 -0600

    Lesson5 (#1)
    * Add channels post action from lesson 4
    
    * Add shortid to to dependencies from lesson 4
    
    * Add delete routes for channels and lessons for Lesson 5
    
    Co-authored-by: patrick_hewes <patrick.hewes@centeva.com>

--- index.js
+++ index.js
@@ -1,14 +1,71 @@
-const express = require("express");
+const express = require('express');
+const shortid = require('shortid');
 
 const server = express();
 
+server.use(express.json());
+
 const PORT = 5000;
+
+let channels = [];
+
+let lessons = [];
 
 // Start by running the following command in the terminal:
 // npm run server
 
 server.get('/', (req, res) => {
     res.json({ hello: "world" });
+});
+
+server.get("/api/channels", (req, res) => {
+    res.status(200).json(channels);
+});
+
+server.post('/api/channels', (req, res) => {
+    const channelInfo = req.body;
+    channelInfo.id = shortid.generate();
+    channels.push(channelInfo);
+    res.status(201).json(channelInfo);
+});
+
+server.delete("/api/channels/:id", (req, res) => {
+    const { id } = req.params;
+
+    const deleted = channels.find(channel => channel.id === id);
+    if (deleted) {
+        channels = channels.find(channel => channel.id !== id);
+        res.status(200).json(deleted);
+    } else {
+        res
+            .status(404)
+            .json({ message: "Channel you are looking for does not exist" });
+    }
+});
+
+server.get("/api/lessons", (req, res) => {
+    res.status(200).json(lessons);
+});
+
+server.post('/api/lessons', (req, res) => {
+    const lessonInfo = req.body;
+    lessonInfo.id = shortid.generate();
+    lessons.push(lessonInfo);
+    res.status(201).json(lessonInfo);
+});
+
+server.delete("/api/lessons/:id", (req, res) => {
+    const { id } = req.params;
+
+    const deleted = lessons.find(lesson => lesson.id === id);
+    if (deleted) {
+        lessons = lessons.find(lesson => lesson.id !== id)
+        res.status(200).json(deleted);
+    } else {
+        res
+            .status(404)
+            .json({ message: "Lesson you are looking for does not exist" });
+    }
 });
 
 server.listen(5000, () => {
